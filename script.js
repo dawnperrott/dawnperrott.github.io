7b55d588985cd96b3cc9f6eb045784f2bba82480
Dawn Perrott <100697701+dawnperrott@users.noreply.github.com>
2022-04-07 15:37:26 +0100

    99 Bottles of Beer
    99 bottles of beer on the wall

--- script.js
+++ script.js
@@ -1,66 +1,9 @@
-function check(){
-	var computerPicks= document.getElementById("compPick").value;
-	var userPicks = document.getElementById("userPick").value;
-	var getResult = document.getElementById("resultCheck").value;
+
+function playSong(){
 	
- 	compPick = generateCompPick();
-
-	console.log(generateCompPick);
-	console.log(compPick);
-	console.log(userPicks);
-	document.getElementById("compPick").innerHTML=compPick;
-
-	if(userPicks==compPick){
-			document.getElementById("resultCheck").innerHTML= "User and Computer draw";
-		}
-		//user picks rock
-		else if(userPicks=="Rock"){
-			if(compPick=="Paper"){
-				document.getElementById("resultCheck").innerHTML="User loses";
-			}
-			else{
-				document.getElementById("resultCheck").innerHTML="User wins";
-			}
-		}
-		//user picks paper
-		else if(userPicks=="Paper"){
-			if(compPick=="Scissors"){
-				document.getElementById("resultCheck").innerHTML= "User loses";
-			}
-			else{
-				document.getElementById("resultCheck").innerHTML="User wins";
-			}
-		}
-		//user picks scissors
-		else if(userPicks=="Scissors"){
-			if(compPick=="Rock"){
-				document.getElementById("resultCheck").innerHTML= "User loses";
-			}
-			else{
-				document.getElementById("resultCheck").innerHTML="User wins";
-
-		}
+	for(i=99;i>=1;i=i-1){
+		document.getElementById("main").innerHTML+="<br/>"+ i +" bottles of beer on the wall, "+i+" bottles of beer\n"
+		+ "Take one down and pass it around, "+(i-1)+"bottles of beer on the wall"+"<br/>";{
 	}
- }
-
-
-function generateCompPick(){
-		const randomNo=Math.floor((Math.random()*3)+1);
-		console.log(randomNo);
-		if(randomNo==1){
-			compPick="Rock"
-		}
-		if(randomNo==2){
-			compPick="Paper"
-		}
-		if(randomNo ==3){
-			compPick="Scissors"
-		
-		}
-		return compPick;
-}
-
-	
-	
-
-	
+  }
+}	
