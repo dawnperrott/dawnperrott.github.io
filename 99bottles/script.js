627070dd179233f89fcecf239c28f9a20fb58ae4
Dawn Perrott <100697701+dawnperrott@users.noreply.github.com>
2022-04-20 20:32:24 +0100

    Updated Conart Gallery

--- 99bottles/script.js
+++
@@ -1,9 +0,0 @@
-
-function playSong(){
-	
-	for(i=99;i>=1;i=i-1){
-		document.getElementById("main").innerHTML+="<br/>"+ i +" bottles of beer on the wall, "+i+" bottles of beer\n"
-		+ "Take one down and pass it around, "+(i-1)+"bottles of beer on the wall"+"<br/>";{
-	}
-  }
-}	
--- README.md
+++
@@ -1,1 +0,0 @@
-# dawnperrott.github.io
